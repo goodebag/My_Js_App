162d71b1e9cb7d6e946295b5c776769f915c274a
Goodebag <egbule.goodluck@gmail.com>
2021-04-22 11:00:59 +0100

    escaping  qoutes

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ let username = Name+" " + x;
 
 //primitives/value type
 let person ={
- myName : 'Goodebag',
+ myName : 'egbule',
  age : 25,
  isApproved :false,
 firstName :undefined
@@ -23,4 +23,9 @@ if('Goodebag'=== namme1['myName']){
 }
 else return 'hello'+' '+ namme1['myName'];
 }
-console.log(reply(person));
+let qoutes = `'here is a qoute = "yeah i just did it"'`;
+let qoutes2 = 'here is a qoute = "yeah i just did it"';
+let qoutes3 = "here is a qoute = \"yeah i just did it\"";
+console.log(qoutes3);
+console.log(qoutes2);
+console.log(qoutes);
