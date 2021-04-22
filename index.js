e3a83c6b7428be70b4e78d695fe892a0fb2387b8
Goodebag <egbule.goodluck@gmail.com>
2021-04-22 03:44:50 +0100

    js syntax

---
+++ index.js
@@ -0,0 +1,16 @@
+let x = "goodluck";
+let Name = "egbule";
+let username = Name+" " + x;
+
+
+//primitives/value type
+let person ={
+ myName : 'Goodebag',
+ age : 25,
+ isApproved :false,
+firstName :undefined
+
+};
+let select = "myName";
+person[select] = 'Chinaza'
+console.log(person['myName']);
